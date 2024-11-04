ee33b8689feea5f76d538963a21a6095b54c1577
ggselectquote <gregory.gushard@selectquote.com>
2024-11-04 07:38:40 -0500

    wip

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **By Andrew and Greg**
 
-![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB) ![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white) ![Espressif](https://img.shields.io/badge/espressif-E7352C.svg?style=for-the-badge&logo=espressif&logoColor=white)
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB) ![Redis](https://img.shields.io/badge/redis-%23DD0031.svg?style=for-the-badge&logo=redis&logoColor=white) ![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white) ![Espressif](https://img.shields.io/badge/espressif-E7352C.svg?style=for-the-badge&logo=espressif&logoColor=white)
 
 ![](__assets/img.png)
 
--- backend/sense-shelf/main/sense-shelf.c
+++ backend/sense-shelf/main/sense-shelf.c
@@ -4,24 +4,32 @@
 #include "driver/gpio.h"
 #include "HX711.h"
 
-#define HX711_DOUT GPIO_NUM_19
-#define HX711_PD_SCK GPIO_NUM_18
+#define HX711_DOUT GPIO_NUM_15
+#define HX711_PD_SCK GPIO_NUM_14
+#define TOLERANCE 0.01
 
 void app_main(void) {
     HX711_init(HX711_DOUT, HX711_PD_SCK, eGAIN_128);
     HX711_tare();
     HX711_set_scale(2280.0);
+
     float last_weight = 0;
-    while(1) {
+
+    while (1) {
         float current_weight = HX711_get_units(5);
-        if (current_weight > last_weight + 0.01) {
-            printf("MORE weight applied: %.2f\n", current_weight);
-        } else if (current_weight < last_weight - 0.01) {
-            printf("LESS weight applied: %.2f\n", current_weight);
-        } else {
-            printf("Weight unchanged: %.2f\n", current_weight);
+        float weight_difference = current_weight - last_weight;
+
+        if (weight_difference > TOLERANCE) {
+            printf("MORE weight applied:\nPrevious Weight: %.2f\nCurrent Weight: %.2f\nDifference: +%.2f\n",
+                   last_weight, current_weight, weight_difference);
+            last_weight = current_weight;
         }
-        last_weight = current_weight;
-        vTaskDelay(pdMS_TO_TICKS(10000));
+        else if (weight_difference < -TOLERANCE) {
+            printf("LESS weight applied:\nPrevious Weight: %.2f\nCurrent Weight: %.2f\nDifference: %.2f\n",
+                   last_weight, current_weight, weight_difference);
+            last_weight = current_weight;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(25));
     }
 }
