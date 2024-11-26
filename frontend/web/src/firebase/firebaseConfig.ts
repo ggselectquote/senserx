574fb94e235a0a41fb897a319f21be9cf33af575
Andrew Pohl <andrew.pohl@selectquote.com>
2024-11-26 11:07:57 -0500

    add firebase config to gitignore

--- frontend/web/src/firebase/firebaseConfig.ts
+++ frontend/web/src/firebase/firebaseConfig.ts
@@ -1,2 +1,8 @@
 export const firebaseConfig = {
+    authDomain: "senserx.firebaseapp.com",
+    apiKey: 'AIzaSyDGDFOwyrrf23IviBKWND8nlp4K3HM_US4',
+    appId: '1:781900009201:android:652246ab0e8a5ecdcbe350',
+    messagingSenderId: '781900009201',
+    projectId: 'senserx',
+    storageBucket: 'senserx.firebasestorage.app',
 };
